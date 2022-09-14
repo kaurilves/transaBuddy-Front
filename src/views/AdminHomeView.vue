0c9409ecc24d68d672683a8987b8775d5591b23f
Siim <moldri.siim@gmail.com>
2022-09-14 15:53:24 +0300

    SIIMM

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    Hello admin
+    <h2>Admin view</h2>
+    <div>
+      <button v-on:click="displayAllUsers" >All users</button>
+      <button v-on:click="displayFindUsers">Search users</button>
+      <button v-on:click="displayAddCustomer">Add user </button>
+      
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: "AdminView"
+  name: "AdminView",
+  data: function () {
+    return{
+
+    }
+  },
+  methods: {
+    displayAllUsers: function () {
+      
+    }
+
+  }
 }
 </script>
+
 
 <style scoped>
 
