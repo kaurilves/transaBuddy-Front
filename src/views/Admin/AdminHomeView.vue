cd39220e10c8939b64339ec0a26f364e94179264
klves <95514872+klves@users.noreply.github.com>
2022-09-19 16:02:44 +0300

    KAUR courierHomeView

--- src/views/Admin/AdminHomeView.vue
+++ src/views/Admin/AdminHomeView.vue
@@ -59,8 +59,7 @@ import UsersByNameAndCodeTable from "@/components/UsersByNameAndCodeTable";
 export default {
   name: "AdminView",
   components: {
-    UserProfile,
-    Logout, AllShipmentPrices, RegisterUser, AddNewUser, UsersByNameAndCodeTable, AllUsersTable,},
+    UserProfile, Logout, AllShipmentPrices, RegisterUser, AddNewUser, UsersByNameAndCodeTable, AllUsersTable,},
   data: function () {
     return{
       user: {},
--- src/views/CourierHomeView.vue
+++ src/views/CourierHomeView.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    Hello courier
+    <h2>Hello courier</h2>
+    <div>
+      <button style="margin: 5px" class="btn btn-outline-dark" v-on:click="displayAllAvailableOrders">Available orders
+      </button>
+      <!--      <button style="margin: 5px" class="btn btn-outline-dark" v-on:click="displayAllUsers" >All users</button>-->
+      <!--      <button style="margin: 5px" class="btn btn-outline-dark" v-on:click="displayAllUsers" >All users</button>-->
+      <!--      <button style="margin: 5px" class="btn btn-outline-dark" v-on:click="displayAllUsers" >All users</button>-->
+    </div>
+    <div v-if="divDisplayAllAvailableOrders">
+      <AllAvailableOrdersTable :orders="orders" title="All available orders"/>
+    </div>
   </div>
 </template>
 
 <script>
+import AllAvailableOrdersTable from "@/components/login/AllAvailableOrdersTable";
+
+
 export default {
-  name: "CourierView"
+  name: "CourierView",
+  components: {
+    AllAvailableOrdersTable
+  },
+  data: function () {
+    return {
+      order: {},
+      orders: [],
+      divDisplayAllAvailableOrders: true,
+      pickUpDistrict: 0,
+      dropOffDistrict: 0,
+      status: 'N'
+    }
+  },
+  methods: {
+    findOrderSByDistrictAndStatus: function () {
+      this.$http.get('/transabuddy/orders/available', {
+            params: {
+              pickUpDistrict: this.pickUpDistrict,
+              dropOffDistrict: this.dropOffDistrict,
+              status: this.status,
+
+            }
+          }
+      ).then(response => {
+        this.orders = response.data
+        console.log(this.orders)
+      })
+    },
+    displayAllAvailableOrders: function (){
+      this.findOrderSByDistrictAndStatus()
+    }
+  }
 }
 </script>
 
