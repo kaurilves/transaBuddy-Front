e1cac75eb979deeb15e558b2e6e3aeb7e22b4262
Siim <moldri.siim@gmail.com>
2022-09-19 16:01:58 +0300

    SIIMM - component naming changed, Admin view updated

--- src/components/NewOrder.vue
+++ src/components/NewOrder.vue
@@ -1,11 +1,174 @@
 <template>
+  <div>
+    <form>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Delivery start time</label>
+        <div class="col-sm-2">
+          <select id="inputStartTime" class="form-control" v-model="this.orderRequest.fromHour">
+            <option disabled value="">Choose start time...</option>
+            <option value="7">0700</option>
+            <option value="8">0800</option>
+            <option value="9">0900</option>
+            <option value="10">1000</option>
+            <option value="11">1100</option>
+            <option value="12">1200</option>
+            <option value="13">1300</option>
+            <option value="14">1400</option>
+            <option value="15">1500</option>
+            <option value="16">1600</option>
+            <option value="17">1700</option>
+            <option value="18">1800</option>
+          </select>
+        </div>
+      </div>
 
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Delivery end time</label>
+        <div class="col-sm-2">
+          <select id="inputEndTime" class="form-control" v-model="this.orderRequest.toHour">
+            <option disabled value="">Choose end time...</option>
+            <option value="7">0700</option>
+            <option value="8">0800</option>
+            <option value="9">0900</option>
+            <option value="10">1000</option>
+            <option value="11">1100</option>
+            <option value="12">1200</option>
+            <option value="13">1300</option>
+            <option value="14">1400</option>
+            <option value="15">1500</option>
+            <option value="16">1600</option>
+            <option value="17">1700</option>
+            <option value="18">1800</option>
+          </select>
+        </div>
+
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Pickup district</label>
+        <div class="col-sm-2">
+          <select id="inputPickupDistrict" class="form-control" v-model="this.orderRequest.pickUpDistrictId">
+            <option disabled value="">Choose pickup district...</option>
+            <option value="1">Mustamäe</option>
+            <option value="2">Lasnamäe</option>
+            <option value="3">Haabersti</option>
+            <option value="4">Nõmme</option>
+            <option value="5">Põhja-Tallinn</option>
+            <option value="6">Kesklinn</option>
+            <option value="7">Kristiine</option>
+            <option value="8">Pirita</option>
+
+          </select>
+        </div>
+
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Pickup address</label>
+          <div class="col-sm-2">
+            <input class="form-control" v-model="this.orderRequest.pickUpAddress" placeholder="Enter pickup address">
+          </div>
+
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Dropoff district</label>
+          <div class="col-sm-2">
+            <select id="inputDropoffDistrict" class="form-control" v-model="this.orderRequest.pickUpDistrictId">
+              <option disabled value="">Choose dropoff district...</option>
+              <option value="1">Mustamäe</option>
+              <option value="2">Lasnamäe</option>
+              <option value="3">Haabersti</option>
+              <option value="4">Nõmme</option>
+              <option value="5">Põhja-Tallinn</option>
+              <option value="6">Kesklinn</option>
+              <option value="7">Kristiine</option>
+              <option value="8">Pirita</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Dropoff address</label>
+          <div class="col-sm-2">
+            <input class="form-control" v-model="this.orderRequest.dropOffAddress" placeholder="Enter dropoff address">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Receiver name</label>
+          <div class="col-sm-2">
+            <input class="form-control" v-model="this.orderRequest.receiverName" placeholder="Enter receiver name">
+          </div>
+
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Receiver phone number</label>
+          <div class="col-sm-2">
+            <input class="form-control" v-model="this.orderRequest.receiverPhoneNumber" placeholder="Enter receiver phone number">
+          </div>
+
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Package amount</label>
+          <div class="col-sm-2">
+            <input class="form-control" v-model="this.orderRequest.packageAmountInShipment" placeholder="Enter package amount">
+          </div>
+
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Package type</label>
+        <div class="col-sm-2">
+          <select id="inputPackageType" class="form-control" v-model="this.orderRequest.shipmentPriceId">
+            <option disabled value="">Choose package size...</option>
+            <option value="1">XS</option>
+            <option value="2">S</option>
+            <option value="3">M</option>
+            <option value="4">L</option>
+            <option value="5">XL</option>
+          </select>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Shipment description</label>
+          <div class="col-sm-2">
+            <input class="form-control" v-model="this.orderRequest.shipmentDescription" placeholder="Enter shipment description">
+          </div>
+
+        </div>
+
+
+
+      </div>
+    </form>
+
+    <input type="text" placeholder="Shipment description" v-model="orderRequest.shipmentDescription"><br><br>
+    <input type="text" placeholder="Comment" v-model="orderRequest.comment"><br><br>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "NewOrder"
+  name: "NewOrder",
+  data: function () {
+    return {
+      orderRequest: {
+        senderUserId: localStorage.getItem('userId'),
+        courierUserId: '',
+        fromHour: '',
+        toHour: '',
+        pickUpDistrictId: '',
+        pickUpAddress: '',
+        dropOffDistrictId: '',
+        dropOffAddress: '',
+        receiverName: '',
+        receiverPhoneNumber: '',
+        packageAmountInShipment: '',
+        shipmentPriceId: '',
+        shipmentDescription: '',
+        comment: '',
+
+      }
+    }
+  },
+
 }
+
+
 </script>
 
 <style scoped>
--- src/views/NewOrderView.vue
+++ src/views/NewOrderView.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <H2>Add new order</H2>
+    <div>
+      <NewOrder/>
+
+    </div>
+    <button type="button" v-on:click="backToAdminView">Back</button>
 
   </div>
 
 </template>
 
 <script>
+import NewOrder from "@/components/NewOrder";
 export default {
-  name: "NewOrderView"
+  name: "NewOrderView",
+  components: {NewOrder},
+  data: function () {
+    return{
+
+    }
+  },
+  methods:{
+    backToAdminView(){
+      this.$router.push({name: 'adminRoute'})
+    }
+  }
 }
 </script>
 
