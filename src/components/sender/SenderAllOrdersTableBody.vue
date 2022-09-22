fc7849ba2c40ab3fcc1e47e8a8444891bfe4fe8e
Eduard <eduard.gorskov@gmail.com>
2022-09-22 13:19:22 +0300

    fix

--- src/components/sender/SenderAllOrdersTableBody.vue
+++ src/components/sender/SenderAllOrdersTableBody.vue
@@ -13,14 +13,7 @@
       </button>
     </td>
   </tr>
-    <select class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
-            aria-expanded="false" v-model="orderInfo.status" v-on:change="findOrdersBySenderUserIdAndStatus">
-      <option value=" "> All </option>
-      <option value="N"> New </option>
-      <option value="A"> Accepted </option>
-      <option value="P"> Picked up </option>
-      <option value="C"> Collected </option>
-    </select>
+
   </tbody>
 </template>
 <script>
@@ -28,42 +21,21 @@
 
 export default {
   name: 'SenderAllOrdersTableBody',
+  props: {
+    orderInfo: {}
+  },
+
   data: function () {
     return {
-
-      userId: this.$route.query.userId,
       orderId: 0,
-      orderInfo:
-          {
-            deliveryDate: '',
-            timeFrame: 0,
-            pickUpAddress: '',
-            dropOffAddress: '',
-            status: ''
-          }
+
     }
   },
   methods: {
-    findOrdersBySenderUserIdAndStatus: function () {
-      this.$http.get("/transabuddy/user/sender/all-orders", {
-            params: {
-              userId: this.userId,
-              status: ' '
-            }
-          }
-      ).then(response => {
-        console.log(response.data)
-        this.orderInfo = response.data
-      }).catch(error => {
-        console.log(error)
-      })
-    },
+
     toOrderView: function (orderId) {
       this.$router.push({name: 'orderView', query: {orderId: orderId}})
     },
-  },
-  mounted() {
-    this.findOrdersBySenderUserIdAndStatus()
   }
 }
 </script>
--- src/views/sender/SenderAllOrdersView.vue
+++ src/views/sender/SenderAllOrdersView.vue
@@ -2,10 +2,17 @@
   <div>
     <div>
       <h1>All orders</h1>
-
+      <select class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
+              aria-expanded="false" v-model="selectedStatus" v-on:change="findOrdersBySenderUserIdAndStatus">
+        <option value=" "> All</option>
+        <option value="N"> New</option>
+        <option value="A"> Accepted</option>
+        <option value="P"> Picked up</option>
+        <option value="C"> Collected</option>
+      </select>
       <table class="table table-hover">
         <SenderAllOrdersTableHeader/>
-        <SenderAllOrdersTableBody/>
+        <SenderAllOrdersTableBody :order-info="orderInfo"/>
       </table>
     </div>
     <div>
@@ -24,10 +31,33 @@ export default {
   components: {SenderAllOrdersTableBody, SenderAllOrdersTableHeader},
   data: function () {
     return {
-
+      userId: this.$route.query.userId,
+      selectedStatus: ' ',
+      orderInfo:
+          {
+            deliveryDate: '',
+            timeFrame: 0,
+            pickUpAddress: '',
+            dropOffAddress: '',
+            status: ''
+          }
     }
   },
   methods: {
+    findOrdersBySenderUserIdAndStatus: function () {
+      this.$http.get("/transabuddy/user/sender/all-orders", {
+            params: {
+              userId: this.userId,
+              status: this.selectedStatus
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+        this.orderInfo = response.data
+      }).catch(error => {
+        console.log(error)
+      })
+    },
     backToMainView() {
 
       const roleSelected = sessionStorage.getItem('roleSelected')
@@ -38,10 +68,12 @@ export default {
       } else {
         this.$router.push({name: 'adminRoute'});
       }
-    },
-
+    }
 
   },
+  mounted() {
+    this.findOrdersBySenderUserIdAndStatus()
+  }
 }
 
 
